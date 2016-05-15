1ace3f485a972b5cb22310d00e3ba6c04dfd1d13
guiltygearxx <guiltygearxx.94@gmail.com>
2016-05-15 10:48:15 +0700

    Toast

--- app/src/main/java/com/example/guiltygearxx/myapplication/MainActivity.java
+++ app/src/main/java/com/example/guiltygearxx/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.guiltygearxx.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"abc",Toast.LENGTH_LONG).show();
     }
 }
